c895885a475c28e954c0a999aa97327af394c5a1
Mnde81 <minubis@gmail.com>
2024-07-25 08:51:46 +0300

    updated file

--- js/components/Layout.js
+++ js/components/Layout.js
@@ -2,17 +2,30 @@ export class Layout {
     constructor() { }
 
     header() {
-        const HTML = '<header>HEADER</header>';
+        const HTML = `<header>
+        <img class="logo" src="./img/logo.png" alt="logo">
+        <nav>
+            <a href="#">Shops</a>
+            <a href="#">Products</a>
+        </nav>
+        </header>`;
         return HTML;
     }
 
     main() {
-        const HTML = '<main>MAIN</main>';
+        const HTML = `<main>
+        <section class="section-1">
+            Section 1
+        </section>
+        <section class="section-2">
+            Section 2
+        </section>
+        </main>`;
         return HTML;
     }
 
     footer() {
-        const HTML = '<footer>FOOTER</footer>';
+        const HTML = `<footer>FOOTER</footer>`;
         return HTML;
     }
 
